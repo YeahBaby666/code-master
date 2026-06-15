5b8dbea119f973c0d2b7695fdf9ca7d2344c0feb
YeahBaby666 <maldo7369@gmail.com>
2026-06-15 13:20:06 -0500

    Ranking Semanal Fix

--- src/main/java/com/codemaster/demo/repository/RankingSemanalRepository.java
+++ src/main/java/com/codemaster/demo/repository/RankingSemanalRepository.java
@@ -3,9 +3,11 @@ package com.codemaster.demo.repository;
 import com.codemaster.demo.model.RankingSemanal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RankingSemanalRepository
         extends JpaRepository<RankingSemanal, UUID> {
-
+// Consulta automática: devuelve todos ordenados por puntos acumulados descendente
+    List<RankingSemanal> findAllByOrderByPuntosAcumuladosDesc();
 }
